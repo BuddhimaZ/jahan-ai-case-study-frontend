f89a6eb9b4c59125eae814ff54061acae6729f8c
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2025-10-13 21:37:10 +0530

    Removed the containers

--- src/main.ts
+++ src/main.ts
@@ -4,36 +4,16 @@ import { isAuthenticated, clearTokens } from "./api/http";
 import { loginView } from "./ui/login";
 import { preferencesView } from "./ui/preferences";
 
-let currentRoot: any = null;
-
-function getAppNode(): HTMLElement {
-  const node = document.getElementById("app");
-  if (!node) throw new Error("#app not found");
-  node.innerHTML = ""; // clear previous markup
-  node.style.minHeight = "100vh";
-  node.style.display = "block";
-  return node;
-}
-
-function showLogin() {
-  const node = getAppNode();
-  if (currentRoot && currentRoot.destructor) currentRoot.destructor();
-  currentRoot = loginView(showPreferences, node);
-}
-
-function showPreferences() {
-  const node = getAppNode();
-  if (currentRoot && currentRoot.destructor) currentRoot.destructor();
-  currentRoot = preferencesView(node);
-}
 
 webix.attachEvent("app:logout", () => {
   clearTokens();
-  showLogin();
+  loginView(preferencesView);
 });
 
-if (isAuthenticated()) {
-  showPreferences();
-} else {
-  showLogin();
-}
+webix.ready(() => {
+  if (isAuthenticated()) {
+    preferencesView();
+  } else {
+    loginView(preferencesView);
+  }
+});
