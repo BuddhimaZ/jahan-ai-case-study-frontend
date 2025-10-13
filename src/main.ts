f5a51070a3df043c8e056d8a76ea1990def7c013
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2025-10-14 05:15:56 +0530

    removed the style.css

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "./style.css";
+// import "./style.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import * as webix from "webix";
 import { isAuthenticated, clearTokens } from "./api/http";
